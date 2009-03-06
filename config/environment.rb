dc37ef8bcd79bbbd1d0aefc7b9a92ec8c8c1e7b3
feedbackmine <feedbackmine@feedbackmine.com>
2009-03-06 00:41:38 -0500

    tomz has merged my changes, deleted feedbackmine-libsvm-ruby, just use tomz's

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', 
     :source => 'http://gems.github.com'
   config.gem 'ferret'
-  config.gem 'feedbackmine-libsvm-ruby', :lib => 'svm', :source => 'http://gems.github.com'
+  config.gem 'tomz-libsvm-ruby-swig', :lib => 'svm', :source => 'http://gems.github.com'
   config.gem 'peterc-whatlanguage', :lib => 'whatlanguage', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
